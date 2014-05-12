78fde490c923abd57c5e5762bf8c2bfe5540c26f
Jan Tatarik <jan.tatarik@gmail.com>
2014-05-12 23:37:28 +0200

    fix comments section

--- magit-gitflow.el
+++ magit-gitflow.el
@@ -22,9 +22,9 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-
+;;
 ;; Gitflow plugin for Magit.
-
+;;
 ;; (require 'magit-gitflow)
 ;; (add-hook 'magit-mode-hook 'turn-on-magit-gitflow)
 ;;
